3db396d25a47a057aefa30a9bc0af43f02ed0cc4
ushanrashmika <ushanrashmika23@gmail.com>
2022-04-25 17:56:01 +0530

    printAns function

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from audioop import mul
+
+
 def add(a,b):
     return a+b
 
@@ -19,11 +22,25 @@ def operation():
 def banner():
     print("======================")
     print("SHELL-CALCULATOR...")
-    oparations=['+ Add','- Reduce','* Multiply','/ Devide']
+    oparations=['+ Add','- Reduce','* Multiply','/ Devide','$ Exit']
     for i in oparations:
         print(i)
     print("======================")
 
+def printAns(data,opp):
+    if(opp=='+'):
+        ans=add(data[0],data[1])
+    elif(opp=='-'):
+        ans=reduce(data[0],data[1])
+    elif(opp='*'):
+        ans=multiply(data[0],data[1])
+    elif(opp=='/'):
+        ans=devide(data[0],data[1])
+    print("{0} {1} {2} = {3}".format(data[0],opp,data[1],ans))
+
 while 1:
     banner()
-
+    o=operation()
+    # check & add exit func
+    data=getData()
+    printAns(data,o)
