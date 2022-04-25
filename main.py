b6a4acb346f1948221d0544fe6afebe81a6549bf
ushanrashmika <ushanrashmika23@gmail.com>
2022-04-25 18:15:13 +0530

    Completed programm

--- main.py
+++ main.py
@@ -14,18 +14,34 @@ def devide(a,b):
     return a/b
 
 def getData():
-    return [int(input("Enter first number: ")),int(input("Enter second number: "))]
+    while 1:
+        d0=input("Enter first number: ")
+        try:
+            d0=int(d0)
+            break
+        except:
+            print("Invalid character. [Enter only numbers]")
+            continue
+    while 1:
+        d1=input("Enter second number: ")
+        try:
+            d1=int(d1)
+            break
+        except:
+            print("Invalid character. [Enter only numbers]")
+            continue
+    return [d0,d1]
 
 def operation():
     return input("Select operation: ")
 
 def banner():
-    print("======================")
+    print("="*22)
     print("SHELL-CALCULATOR...")
     oparations=['+ Add','- Reduce','* Multiply','/ Devide','$ Exit']
     for i in oparations:
         print(i)
-    print("======================")
+    print("="*22)
 
 def printAns(data,opp):
     if(opp=='+'):
@@ -41,7 +57,14 @@ def printAns(data,opp):
 while 1:
     banner()
     o=operation()
-    if(o=='$'):
-        print(".!.!................!.!.")
+    if(o in ['+','-','*','/','$']):
+        if(o=='$'):
+            print(".!.!........EXIT........!.!.")
+            exit()
+    else:
+        print("Invalid Operation!.!.!")
+        continue
     data=getData()
     printAns(data,o)
+    input("=>> Press Any Key To Continue")
+    print("_"*22)
