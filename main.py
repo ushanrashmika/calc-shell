24e96403ef5982ca48de946d6629f8220d9eddc0
ushanrashmika <ushanrashmika23@gmail.com>
2022-04-25 17:41:31 +0530

    banner lines writed

--- main.py
+++ main.py
@@ -13,11 +13,17 @@ def devide(a,b):
 def getData():
     return [int(input("Enter first number: ")),int(input("Enter second number: "))]
 
-def operation(o):
+def operation():
     return input("Select operation: ")
 
 def banner():
+    print("======================")
     print("SHELL-CALCULATOR...")
+    oparations=['+ Add','- Reduce','* Multiply','/ Devide']
+    for i in oparations:
+        print(i)
+    print("======================")
 
 while 1:
     banner()
+
